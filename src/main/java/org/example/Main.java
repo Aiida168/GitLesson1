aa938864b0a86c3c378a5a2317707697eb1ad810
Aida <aiida@list.ru>
2023-04-04 16:46:18 +0600

    created new project for able workshop

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessageAble();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodShowMessageAble() {
+            System.out.println("hello Viva Omir\n");
+    }
+    private static void mainMethodShowInfo() {
+        System.out.println("Lets learn Git");
+    }
+
+}
